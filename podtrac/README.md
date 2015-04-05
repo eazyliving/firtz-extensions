469a835f38dd4e4a4504aa7494816e8326c74b6d
Christian Bednarek <eazy@eazy.amigager.de>
2015-04-06 01:35:03 +0200

    added podtrac

---
+++ podtrac/README.md
@@ -0,0 +1,2 @@
+podtrac extension to change links for podtrac-tracking.
+no action required, no variables
---
+++ podtrac/podtrac.php
@@ -0,0 +1,43 @@
+<?php
+
+
+function podtrac_episode($item) {
+
+	global $main;
+	if ($main->get('version')<2) return $item;
+	
+	# <repeat group="{{@item.audiofiles}}" value="{{@audiofile}}" key="{{@audiotype}}">
+	
+	foreach ($item['audiofiles'] as $key=>$audio) {
+		$redir = '';
+		switch ($key) {
+		
+			case 'mp3':
+				$redir = 'mp3';
+				break;
+				
+			case 'm4a':
+				$redir = 'aac';
+				break;
+				
+			case 'opus':
+			case 'ogg':
+				$redir = 'ogg';
+				break;
+				
+			default:
+				return $item;
+				
+		
+		}
+		$info = parse_url($audio['link']);
+		$newlink = str_replace($info['scheme'].'://','',$audio['link']);
+		$item['audiofiles'][$key]['link']='http://www.podtrac.com/pts/redirect.'.$redir.'/'.$newlink;
+	
+	}	
+	return $item;
+	
+}
+
+
+?>
